949fb91cc8e8300db7ee48b1b3b8f74b6f7e3199
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 00:19:22 +0800

    [PRACTICE11][Cedric Coloma] refactor: Make Observer and Observable contain different types of data.

--- src/main/java/practice11/Observable.java
+++ src/main/java/practice11/Observable.java
@@ -2,6 +2,6 @@ package practice11;
 
 public interface Observable {
     public void registerObserver(Observer observer);
-    public void notifyObservers(Student student, String action);
+    public void notifyObservers(EventData eventData, Event event);
     public void removeObserver(Observer observer);
 }
